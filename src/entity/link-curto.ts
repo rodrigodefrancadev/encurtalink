83b932d22263a3a3998fb721d2b43ee5b7a69574
Rodrigo de França - DEV <159470915+rodrigodefrancadev@users.noreply.github.com>
2024-05-22 14:27:11 +0000

    added link-curto entity

---
+++ src/entity/link-curto.ts
@@ -0,0 +1,35 @@
+interface LinkCurtoProps {
+  id: number;
+  link: string;
+  criadoEm: Date;
+}
+
+export class LinkCurto implements LinkCurtoProps {
+  public readonly id: number;
+  public readonly link: string;
+  public readonly criadoEm: Date;
+  public get slug(): string {
+    return this.generateSlug();
+  }
+
+  constructor(props: LinkCurtoProps) {
+    this.id = props.id;
+    this.link = props.link;
+    this.criadoEm = props.criadoEm;
+  }
+
+  private generateSlug() {
+    return "G" + this.id.toString(16);
+  }
+
+  public static decodeSlug(slug: string) {
+    if (!slug.startsWith("G")) {
+      throw new Error("Invalid Slug");
+    }
+
+    const hex = slug.substring(1);
+    const id = parseInt(hex, 16);
+
+    return id;
+  }
+}
