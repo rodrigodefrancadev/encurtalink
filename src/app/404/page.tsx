9deb7db3becb0aae07e5046aaf0ce8ddca58b0c9
Rodrigo de França - DEV <159470915+rodrigodefrancadev@users.noreply.github.com>
2024-05-30 13:20:16 +0000

    enhence styles of page 404

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,17 +1,34 @@
 "use client";
 import { FunctionComponent, Suspense } from "react";
-import styles from "./style.module.css";
 import { useSearchParams } from "next/navigation";
+import { Box, Text } from "@chakra-ui/react";
+import { getShortUrlBySlug } from "../_common/get-short-url-by-slug";
 
 const Page404: FunctionComponent = () => {
   const params = useSearchParams();
   const slug = params.get("slug");
   return (
-    <div className={styles.container}>
-      <div className={styles.container404}>404</div>
-      {slug && <div className={styles.slug}>{slug}</div>}
-      <div>Página não encontrada.</div>
-    </div>
+    <Box display={"grid"} placeItems="center" height="100vh" width="100vw">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        width="100%"
+        maxWidth={400}
+      >
+        <Text fontSize={"xxx-large"} fontWeight={"500"}>
+          404
+        </Text>
+        {slug && (
+          <Box backgroundColor={"white"} mt={4} py={1} px={2} borderRadius={8}>
+            <Text>{getShortUrlBySlug(slug)}</Text>
+          </Box>
+        )}
+        <Text fontSize={"large"} mt={4}>
+          Página não encontrada!
+        </Text>
+      </Box>
+    </Box>
   );
 };
 
